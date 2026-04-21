4a536439ae852509c69c1dc41686587fb1488a11
Jacob-J-Nealy <jacobnealy01@gmail.com>
2026-04-21 12:20:37 -0400

    Added Modifer to GreetingText()

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
 }
 // Added New Function
 @Composable
-fun GreetingText() {
+fun GreetingText(modifier: Modifier = Modifier) {
 }
 
 @Preview(showBackground = true)
