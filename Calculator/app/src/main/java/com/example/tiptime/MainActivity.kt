ebdf4752752325864b011181eb38406a5db6e35b
Jacob-J-Nealy <jacobnealy01@gmail.com>
2026-05-07 11:38:29 -0400

    Finished Module 3

--- Calculator/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -50,6 +50,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TipTimeLayout() {
+    var amountInput by remember { mutableStateOf("") }
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
+
     Column(
         modifier = Modifier
             .statusBarsPadding()
@@ -64,15 +68,16 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
-
         EditNumberField(
+            value = amountInput,
+            onValueChange = { amountInput = it },
             modifier = Modifier
                 .padding(bottom = 32.dp)
                 .fillMaxWidth()
         )
 
         Text(
-            text = stringResource(R.string.tip_amount, "$0.00"),
+            text = stringResource(R.string.tip_amount, tip),
             style = MaterialTheme.typography.displaySmall
         )
 
@@ -85,10 +90,6 @@ fun EditNumberField(
     value: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier) {
-    var amountInput by remember { mutableStateOf("")
-    }
-    val amount = amountInput.toDoubleOrNull() ?: 0.0
-    val tip = calculateTip(amount)
     TextField(
         value = value,
         onValueChange = onValueChange,
