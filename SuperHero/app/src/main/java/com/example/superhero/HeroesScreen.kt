3701b4c687a5af93f8ce8f3004a3d9adcec6ce52
Jacob-J-Nealy <jacobnealy01@gmail.com>
2026-05-09 03:02:14 -0400

    Don't remember what I was doing but...I know I am almost done

--- SuperHero/app/src/main/java/com/example/superhero/HeroesScreen.kt
+++ SuperHero/app/src/main/java/com/example/superhero/HeroesScreen.kt
@@ -1,4 +1,85 @@
-package com.example.superhero
+package com.example.superheroes
 
-class HeroesScreen {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.superheroes.model.Hero
+import com.example.superheroes.model.HeroesRepository
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CardDefaults
+
+@Composable
+fun HeroListItem(
+    hero: Hero,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp),
+        elevation = CardDefaults.cardElevation(2.dp),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = stringResource(hero.nameRes),
+                    style = MaterialTheme.typography.displaySmall,
+                    maxLines = 1
+                )
+
+                Text(
+                    text = stringResource(hero.descriptionRes),
+                    style = MaterialTheme.typography.bodyLarge,
+                    maxLines = 2
+                )
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Image(
+                painter = painterResource(hero.imageRes),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(72.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+}
+
+@Composable
+fun HeroesScreen() {
+    LazyColumn(
+        contentPadding = PaddingValues(
+            vertical = 8.dp,
+            horizontal = 16.dp
+        ),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(HeroesRepository.heroes) { hero ->
+            HeroListItem(hero = hero)
+        }
+    }
 }
--- SuperHero/app/src/main/java/com/example/superhero/MainActivity.kt
+++ SuperHero/app/src/main/java/com/example/superhero/MainActivity.kt
@@ -11,20 +11,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.superhero.ui.theme.SuperHeroTheme
+import com.example.superhero.ui.theme.SuperheroesTheme
+import com.example.superheroes.HeroesScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SuperHeroTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            SuperheroesTheme() {
+                HeroesScreen()
             }
         }
     }
@@ -41,7 +37,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    SuperHeroTheme {
+    SuperheroesTheme() {
         Greeting("Android")
     }
 }
