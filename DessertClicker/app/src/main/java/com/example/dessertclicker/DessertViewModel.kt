b5e9a2af2b52f4875ea4bfa770e454161ec510a1
Jacob-J-Nealy <jacobnealy01@gmail.com>
2026-05-10 05:07:17 -0400

    Added Onclicked function for Dessert

--- DessertClicker/app/src/main/java/com/example/dessertclicker/DessertViewModel.kt
+++ DessertClicker/app/src/main/java/com/example/dessertclicker/DessertViewModel.kt
@@ -1,8 +1,15 @@
 package com.example.dessertclicker
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 
 
 class DessertViewModel : ViewModel() {
 
+    var uiState by mutableStateOf(DessertUiState())
+        private set
+
+    fun onDessertClicked(price: Int) {
+    }
 }
