b0cf191ca002e3a42a932285d88c7a9176349fe9
Jacob-J-Nealy <jacobnealy01@gmail.com>
2026-05-10 06:33:22 -0400

    updated DAO

--- BusSchedule/app/src/main/java/com/example/busschedule/data/BusScheduleDao.kt
+++ BusSchedule/app/src/main/java/com/example/busschedule/data/BusScheduleDao.kt
@@ -25,20 +25,17 @@ import kotlinx.coroutines.flow.Flow
  */
 @Dao
 interface BusScheduleDao {
-    @Query(
-        """
-        SELECT * FROM schedule 
-        ORDER BY arrival_time ASC    
-        """
-    )
+
+    @Query("""
+        SELECT * FROM bus_schedule
+        ORDER BY arrival_time ASC
+    """)
     fun getAll(): Flow<List<BusSchedule>>
 
-    @Query(
-        """
-        SELECT * FROM schedule 
-        WHERE stop_name = :stopName 
-        ORDER BY arrival_time ASC 
-        """
-    )
+    @Query("""
+        SELECT * FROM bus_schedule
+        WHERE stop_name = :stopName
+        ORDER BY arrival_time ASC
+    """)
     fun getByStopName(stopName: String): Flow<List<BusSchedule>>
 }
